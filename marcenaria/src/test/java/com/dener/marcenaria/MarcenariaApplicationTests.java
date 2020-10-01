c23c4d934a4732118e5eb49323f3251ff709cd06
denercanal <denercanal@hotmail.com>
2020-10-01 02:14:40 -0300

    projeto criado

---
+++ marcenaria/src/test/java/com/dener/marcenaria/MarcenariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dener.marcenaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcenariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
